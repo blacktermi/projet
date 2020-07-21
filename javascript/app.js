a465b74d26e6f80386fe9e0ef6eae10cf0051d7e
joseph kakou <blacktermi@protmail.com>
2020-07-21 17:28:47 +0000

    fin de fenetre modal

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,38 @@
-const openModal = function(e) {
+let modal = null
+
+const ouvrirModal = function(e) {
     e.preventDefault()
     const target = document.querySelector(e.target.getAttribute('href'))
     target.style.display = null
     target.removeAttribute('aria-hidden')
     target.setAttribute('aria-modal', 'true')
+    modal = target
+    modal.addEventListener('click', fermerModal)
+    modal.querySelector('.js-close-modal').addEventListener('click', fermerModal )
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation )
 }
 
+const fermerModal = function(e) {
+    if(modal === null) return
+    e.preventDefault()
+    modal.style.display = "none"
+    modal.setAttribute('aria-hidden', true)
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', fermerModal)
+    modal.querySelector('.js-close-modal').removeEventListener('click', fermerModal )
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation )
+    modal = null
+}
+
+const stopPropagation =function(e){
+    e.stopPropagation()
+}
 
 document.querySelectorAll('.js-modal').forEach(a => {
-    a.addEventListener('click', openModal) 
+    a.addEventListener('click', ouvrirModal) 
+})
+window.addEventListener('keydown', function(e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        fermerModal(e)
+    }
 })
